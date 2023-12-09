5d18bf43fb596d245ac41675871f5882e045735f
doinel1a <doinel1atanasiu@gmail.com>
2023-12-09 21:44:29 +0100

    feat: implement & configure wagmi and web3 modal

--- front-end/src/app.tsx
+++ front-end/src/app.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { WagmiConfig } from 'wagmi';
+import { arbitrum, avalanche, bsc, fantom, gnosis, mainnet, optimism, polygon } from 'wagmi/chains';
 
 import mainBG from '@/assets/images/main-bg.svg';
 import { Toaster } from '@/components/ui/toaster';
@@ -11,29 +14,43 @@ import ThemeProvider from './components/ui/theme/provider';
 import routes from './config/routes';
 import EStorageKeys from './constants/storage-keys';
 
+const chains = [mainnet, polygon, avalanche, arbitrum, bsc, optimism, gnosis, fantom];
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || '';
+const metadata = {
+  name: 'DeFi Builder',
+  description: 'DeFi Builder',
+  url: 'https://web3modal.com',
+  icons: ['https://avatars.githubusercontent.com/u/142919060']
+};
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+createWeb3Modal({ wagmiConfig, projectId, chains });
 function App() {
   return (
     <BrowserRouter>
-      <ThemeProvider defaultTheme='system' storageKey={EStorageKeys.theme}>
-        <Navbar />
+      <WagmiConfig config={wagmiConfig}>
+        <ThemeProvider defaultTheme='system' storageKey={EStorageKeys.theme}>
+          <Navbar />
 
-        <main
-          className='flex h-full w-full justify-center pb-20 pt-40'
-          style={{
-            background: `url(${mainBG}) no-repeat center top`
-          }}
-        >
-          <Routes>
-            {routes.map((route) => (
-              <Route key={`route-${route.path}`} path={route.path} element={<route.page />} />
-            ))}
-          </Routes>
-        </main>
+          <main
+            className='flex h-full w-full justify-center pb-20 pt-40'
+            style={{
+              background: `url(${mainBG}) no-repeat center top`
+            }}
+          >
+            <Routes>
+              {routes.map((route) => (
+                <Route key={`route-${route.path}`} path={route.path} element={<route.page />} />
+              ))}
+            </Routes>
+          </main>
 
-        <Footer />
+          <Footer />
 
-        <Toaster />
-      </ThemeProvider>
+          <Toaster />
+        </ThemeProvider>
+      </WagmiConfig>
     </BrowserRouter>
   );
 }
